d6ba3d34fd22ac50e6e82fa0c01933ef9b5e43b4
vince0656 <vince0656@googlemail.com>
2019-09-23 13:25:38 +0100

    Basic shell going

---
+++ src/components/Bid.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Auction bid</h1>
+    <hr/>
+    <form>
+      <div>
+        <label for="parameter-input">
+          Parameter:
+          <input id="parameter-input" type="number" v-model="parameter" />
+        </label>
+      </div>
+      <div>
+        <label for="bid-amount-input">
+          Bid amount:
+          <input id="bid-amount-input" type="number" v-model="bid" />
+        </label>
+      </div>
+      <input type="submit"/>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Bid',
+  data: () => {
+    return {
+      parameter: 0,
+      bid: 0
+    };
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Bid />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Bid from '@/components/Bid.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'home',
   components: {
-    HelloWorld,
+    Bid,
   },
 });
 </script>
+
+<style scoped>
+  .home {
+    padding: 0 35px;
+  }
+</style>
