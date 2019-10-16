a9fc6b3d78933d5154b28acbfee800fc787c9ca0
juhu92 <hummju@gmail.com>
2019-10-16 18:42:34 +0200

    changed about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,26 +2,26 @@
   <div class="about container">
     
     <h1 class="mb-4">About</h1>
-    <p class="text-large mb-6">Twistedsister is the eponymous name of a Switzerland-based temporary collective of artists and designers investigating the potentials of digital technologies and their cultural impact. Twistedsister is <a href="https://studio.arminblasbichler.com">Armin Blasbichler</a>, <a href="https://www.brigittefaessler.com">Brigitte Fässler</a>, <a href="https://www.schreibtgenau.ch">Martina Graf</a>, <a href="https://julianhumm.com">Julian Humm</a> and <a href="http://www.deixis.ch">Matthias Pfäffli</a>.</p>
+    <p class="text-large mb-6">Twistedsister is the eponymous name of a Switzerland-based temporary collective of artists and designers investigating the potentials of digital technologies and their cultural impact. Twistedsister is <a target="_blank" href="https://studio.arminblasbichler.com">Armin Blasbichler</a>, <a target="_blank" href="https://www.brigittefaessler.com">Brigitte Fässler</a>, <a target="_blank" href="https://www.schreibtgenau.ch">Martina Graf</a>, <a target="_blank" href="https://julianhumm.com">Julian Humm</a> and <a target="_blank" href="http://www.deixis.ch">Matthias Pfäffli</a>.</p>
 
 
 
     <div class="row mb-6">
 
     	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="mailto:hello@twistedsister.io" class="btn btn-secondary text-white w-100">Email</a></span>
+    		<span><a href="mailto:hello@twistedsister.io" target="_blank" class="btn btn-secondary text-white w-100">Email</a></span>
     	</div>
 
     	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://twitter.com/TwistedsisterI" class="btn btn-secondary text-white w-100">Twitter</a></span>
+    		<span><a href="https://twitter.com/TwistedsisterI" target="_blank" class="btn btn-secondary text-white w-100">Twitter</a></span>
     	</div>
 
     	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://www.instagram.com/twistedsister.io" class="btn btn-secondary text-white w-100">Instagram</a></span>
+    		<span><a href="https://www.instagram.com/twistedsister.io" target="_blank" class="btn btn-secondary text-white w-100">Instagram</a></span>
     	</div>
 
     	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://www.facebook.com/twistedsister.io/" class="btn btn-secondary text-white w-100">Facebook</a></span>
+    		<span><a href="https://www.facebook.com/twistedsister.io/" target="_blank" class="btn btn-secondary text-white w-100">Facebook</a></span>
     	</div>
 
     </div>
@@ -32,11 +32,11 @@
     <div class="row mb-6">
 
     	<div class="col-12 col-md-3 offset-md-3 text-center mb-2">
-    		<span><a href="https://s3.eu-central-1.amazonaws.com/twistedsister.io/Pressetext_dt_final_web.pdf" class="btn btn-light w-100">Pressemitteilung</a></span>
+    		<span><a href="https://s3.eu-central-1.amazonaws.com/twistedsister.io/Pressetext_dt_final_web.pdf" target="_blank" class="btn btn-light w-100">Pressemitteilung</a></span>
     	</div>
 
     	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://s3.eu-central-1.amazonaws.com/twistedsister.io/Pressetext_en_final_web.pdf" class="btn btn-light w-100">Press Release</a></span>
+    		<span><a href="https://s3.eu-central-1.amazonaws.com/twistedsister.io/Pressetext_en_final_web.pdf" target="_blank" class="btn btn-light w-100">Press Release</a></span>
     	</div>
 
 
@@ -50,11 +50,11 @@
     
     <div class="text-center">
     		
-    		<a href="https://www.cms-basel.ch" class="mb-5 d-inline-block">
+    		<a href="https://www.cms-basel.ch" target="_blank" class="mb-5 d-inline-block">
 	    		<img src="../assets/cms_Logo_sw.png" class="supporter-logo">
     		</a>
 
-    		<a href="https://www.kultur.bs.ch/kulturprojekte/film-und-medienkunst/fachausschuss-film-und-medienkunst-bs-bl.html" class="mb-5 d-inline-block">
+    		<a href="https://www.kultur.bs.ch/kulturprojekte/film-und-medienkunst/fachausschuss-film-und-medienkunst-bs-bl.html" target="_blank" class="mb-5 d-inline-block">
 	    		<img src="../assets/FA_BSBL.png" class="supporter-logo ">
     		</a>
 
