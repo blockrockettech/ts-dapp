72916ff8d7e011dd6fdf688dd8a4ae3dde6d198a
Andy Gray <andy@whatsthescore.co.uk>
2020-01-23 09:44:06 +0000

    faq updates

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -64,11 +64,7 @@
                 What happens after the genesis auction?
             </template>
             <template v-slot:body>
-                <p>With the end of the genesis auction the appearance of twistedsister is finalized and all tokens will
-                    have been distributed. Holding a TWIST token represents a tamperproof certificate of participation
-                    in and ownership of a limited-edition artwork. A total of 21 TWIST tokens will be issued. Each token
-                    is numbered. TWIST token holders will have the right to access, view and download the jointly
-                    modified avatar of twistedsister.</p>
+                <p>With the end of the genesis auction the appearance of twistedsister is finalized and all tokens will have been distributed. Holding a TWIST token represents a tamperproof certificate of participation in and ownership of a limited-edition artwork. A total of 21 TWIST tokens will be issued.</p>
             </template>
         </FAQContainer>
 
@@ -78,12 +74,7 @@
                 twistedsister: “What is going to happen with me eventually?”
             </template>
             <template v-slot:body>
-                <p>A scaled version of the final 3D-model will be 3D-printed by the artists. The resulting statue
-                    represents the unique physical edition of the artwork and will cryptographically be linked to a
-                    single ERC-721 token, the TWIST3D token. In a one-off IRL exhibition in January 2020, the artists
-                    reserve the right to present, perform and eventually sell the statue on behalf of the TWIST token
-                    holders. Once the sale is confirmed on the blockchain, TWIST and TWIST3D token holders will be able
-                    to access, view and download the twistedsister 3d-model.</p>
+                <p>A scaled version of the final 3D-model will be printed by the artists. The resulting statue represents the physical edition of the co-created artwork and is cryptographically linked to a unique ERC-721 token – the TWIST3D token. In January 2020 the artists will sell the statue on behalf of the TWIST token holders. After the sale TWIST and TWIST3D token holders will be able to trade their items.</p>
             </template>
         </FAQContainer>
 
@@ -93,13 +84,7 @@
                 What can I do with my tokens?
             </template>
             <template v-slot:body>
-                <p>TWIST token holders can sell, trade and transfer their digital items with other users directly on
-                    the upcoming <a href="//twistedsister.io">twistedsister.io</a> marketplace or on secondary marketplaces such as opensea.io. The
-                    TWIST3D token holder is the legitimate owner of the unique physical edition of twistedsister, while
-                    the TWIST token owners are its stakeholders. In order to maintain the link between the physical and
-                    the digital domain, it is the TWIST3D token holder’s responsibility to transact any IRL change of
-                    ownership of the statue (sell, trade, transfer) by using the smart contract address integrated at
-                    the bottom of the statue’s base. The same responsibility applies to vice-versa transactions.</p>
+                <p>TWIST token holders can trade their items on the twistedsister.io marketplace. The 3D-model will be made available for download for everyone to play with, to copy, to print. The TWIST3D token holder is the only legitimate owner of the physical edition of twistedsister. </p>
             </template>
         </FAQContainer>
 
@@ -124,11 +109,7 @@
                 Automated funds distribution, say what?
             </template>
             <template v-slot:body>
-                <p>Open blockchains facilitate permissionless, secure, near instant peer-to-peer value transactions
-                    without an intermediary. Automated funds distribution via smart contracts is a central part of the
-                    <a href="//twistedsister.io">twistedsister.io</a> project. It allows a fair-minded value chain
-                    design. In secondary market sales 20% of a transaction will equally be distributed among the token
-                    holders, while 10% will be equally distributed among the artists.</p>
+                <p>The twistedsister.io marketplace has been devised with a fair future value attribution in mind. For each sale of a TWIST token, 20% of the money will equally be split among the TWIST token holders, and 10% will equally be split among the artists.</p>
             </template>
         </FAQContainer>
 
