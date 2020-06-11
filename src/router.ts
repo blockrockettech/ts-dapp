8a254c0e9964fc3ca206193a00a14f4a01f0d07b
riobasel <hello@riobasel.ch>
2020-06-11 16:37:03 +0200

    added Documentation.vue page

--- src/router.ts
+++ src/router.ts
@@ -22,6 +22,14 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/Concept.vue')
     },
     {
+      path: '/documentation',
+      name: 'documentation',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Documentation.vue')
+    },
+    {
       path: '/genesis-auction',
       name: 'genesis-auction',
       component: Countdown
---
+++ src/views/Documentation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+
+        <h5>EN</h5>
+        <div class="embed-responsive embed-responsive-16by9 mb-5">
+            <iframe
+                    src="https://player.vimeo.com/video/383586346"
+                    width="640"
+                    height="360"
+                    frameborder="0"
+                    allow="autoplay;
+                    fullscreen"
+                    allowfullscreen
+            />
+        </div>
+
+        <h5>DE</h5>
+        <div class="embed-responsive embed-responsive-16by9 mb-5 ">
+            <iframe
+                    src="https://player.vimeo.com/video/401900274"
+                    width="640"
+                    height="360"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+            />
+        </div>
+
+
+        <p class="text-large">
+            <a href="http://twistedsister.io/content/twistedsister.io_Projektdokumentation.pdf">Download Documentation (DE)</a>
+        </p>
+
+
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: "concept"
+    });
+</script>
+
+<style scoped>
+
+</style>
