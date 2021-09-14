3069eb55627162d30717aa9c7eeb698f3d10fd12
juhu92 <julian@riobasel.ch>
2021-09-14 19:27:57 +0200

    changed about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,74 +12,100 @@
           allowfullscreen
       />
     </div>
-    
-    <h1 class="mb-4">About</h1>
-    <p class="text-large mb-6">Twistedsister is the eponymous name of a Switzerland-based temporary collective of artists and designers investigating the potentials of digital technologies and their cultural impact. Twistedsister is <a target="_blank" href="https://studio.arminblasbichler.com">Armin Blasbichler</a>, <a target="_blank" href="https://www.brigittefaessler.com">Brigitte Fässler</a>, <a target="_blank" href="https://www.schreibtgenau.ch">Martina Graf</a>, <a target="_blank" href="https://julianhumm.com">Julian Humm</a> and <a target="_blank" href="http://www.deixis.ch">Matthias Pfäffli</a>.</p>
 
+    <h1 class="mb-4">About</h1>
+    <p class="text-large mb-6">Twistedsister is the eponymous name of a Switzerland-based temporary collective of
+      artists and designers investigating the potentials of digital technologies and their cultural impact.
+      Twistedsister is <a target="_blank" href="https://studio.arminblasbichler.com">Armin Blasbichler</a>, <a
+          target="_blank" href="https://www.brigittefaessler.com">Brigitte Fässler</a>, <a target="_blank"
+                                                                                           href="https://www.schreibtgenau.ch">Martina
+        Graf</a>, <a target="_blank" href="https://julianhumm.com">Julian Humm</a> and <a target="_blank"
+                                                                                          href="http://www.deixis.ch">Matthias
+        Pfäffli</a>.</p>
 
 
     <div class="row mb-6">
 
-    	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="mailto:hello@twistedsister.io" target="_blank" class="btn btn-secondary text-white w-100">Email</a></span>
-    	</div>
+      <div class="col-12 col-md-3 text-center mb-2">
+        <span><a href="mailto:hello@twistedsister.io" target="_blank"
+                 class="btn btn-secondary text-white w-100">Email</a></span>
+      </div>
 
-    	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://twitter.com/twistedsisterio" target="_blank" class="btn btn-secondary text-white w-100">Twitter</a></span>
-    	</div>
+      <div class="col-12 col-md-3 text-center mb-2">
+        <span><a href="https://twitter.com/twistedsisterio" target="_blank" class="btn btn-secondary text-white w-100">Twitter</a></span>
+      </div>
 
-    	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://www.instagram.com/twistedsister.io" target="_blank" class="btn btn-secondary text-white w-100">Instagram</a></span>
-    	</div>
+      <div class="col-12 col-md-3 text-center mb-2">
+        <span><a href="https://www.instagram.com/twistedsister.io" target="_blank"
+                 class="btn btn-secondary text-white w-100">Instagram</a></span>
+      </div>
 
-    	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://www.facebook.com/twistedsister.io/" target="_blank" class="btn btn-secondary text-white w-100">Facebook</a></span>
-    	</div>
+      <div class="col-12 col-md-3 text-center mb-2">
+        <span><a href="https://www.facebook.com/twistedsister.io/" target="_blank"
+                 class="btn btn-secondary text-white w-100">Facebook</a></span>
+      </div>
 
     </div>
-    
-
 
 
     <div class="row mb-6">
 
-    	<div class="col-12 col-md-3 offset-md-3 text-center mb-2">
-    		<span><a href="https://ipfs.globalupload.io/QmZHi11L4HzdP41L8KqSPvptmjsvkGqrHpXG9hJqg3JCsT" target="_blank" class="btn btn-light w-100">Pressemitteilung</a></span>
-    	</div>
+      <div class="col-12 col-md-3 text-center mb-2">
+    		<span>
+          <a href="https://knownorigin.io/twistedsisterio" target="_blank" class="btn btn-secondary text-white w-100">
+            knownorigin.io
+          </a>
+        </span>
+      </div>
 
-    	<div class="col-12 col-md-3 text-center mb-2">
-    		<span><a href="https://ipfs.globalupload.io/QmXWSuMrqi6oBWsXadGSbiRGbFiZsertn43JQUB8mud8Z7" target="_blank" class="btn btn-light w-100">Press Release</a></span>
-    	</div>
+      <div class="col-12 col-md-3 text-center mb-2">
+        <span>
+          <a
+              href="https://ipfs.globalupload.io/QmZHi11L4HzdP41L8KqSPvptmjsvkGqrHpXG9hJqg3JCsT"
+              target="_blank"
+              class="btn btn-light w-100">
+            Pressemitteilung
+          </a>
+        </span>
+      </div>
+
+      <div class="col-12 col-md-3 text-center mb-2">
+        <span>
+          <a
+              href="https://ipfs.globalupload.io/QmXWSuMrqi6oBWsXadGSbiRGbFiZsertn43JQUB8mud8Z7"
+              target="_blank"
+             class="btn btn-light w-100">Press Release</a>
+        </span>
+      </div>
 
 
     </div>
 
 
     <h6 class="text-center mb-3">Supported by</h6>
-    
-    <div class="text-center">
-    		
-    		<a href="https://www.cms-basel.ch" target="_blank" class="mb-5 d-inline-block">
-	    		<img src="../assets/cms_Logo_sw.png" class="supporter-logo">
-    		</a>
 
-    		<a href="https://www.kultur.bs.ch/kulturprojekte/film-und-medienkunst/fachausschuss-film-und-medienkunst-bs-bl.html" target="_blank" class="mb-5 d-inline-block">
-	    		<img src="../assets/FA_BSBL.png" class="supporter-logo ">
-    		</a>
+    <div class="text-center">
+
+      <a href="https://www.cms-basel.ch" target="_blank" class="mb-5 d-inline-block">
+        <img src="../assets/cms_Logo_sw.png" class="supporter-logo">
+      </a>
+
+      <a href="https://www.kultur.bs.ch/kulturprojekte/film-und-medienkunst/fachausschuss-film-und-medienkunst-bs-bl.html"
+         target="_blank" class="mb-5 d-inline-block">
+        <img src="../assets/FA_BSBL.png" class="supporter-logo ">
+      </a>
 
     </div>
-
-
 
 
   </div>
 </template>
 
 <style scoped>
-  .supporter-logo{
-    display: inline-block;
-    width: 100%; 
-    max-width: 250px;
-    margin: 0 2rem ;
-  }
+.supporter-logo {
+  display: inline-block;
+  width: 100%;
+  max-width: 250px;
+  margin: 0 2rem;
+}
 </style>
